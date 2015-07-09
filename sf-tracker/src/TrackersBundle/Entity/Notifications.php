f11cabce3fef6b694af002d74d4f146e859bd5ae
vinhne <phamquocvinh99@gmail.com>
2015-07-09 16:26:06 +0700

    update profile

---
+++ sf-tracker/src/TrackersBundle/Entity/Notifications.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace TrackersBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Notifications
+ *
+ * @ORM\Table(name="project_notifications")
+ * @ORM\Entity
+ */
+class Notifications
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="issue_id", type="integer")
+     */
+    private $issueId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="project_id", type="integer")
+     */
+    private $projectId;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_read", type="boolean")
+     */
+    private $isRead;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modified", type="datetime")
+     */
+    private $modified;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Notifications
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set issueId
+     *
+     * @param integer $issueId
+     * @return Notifications
+     */
+    public function setIssueId($issueId)
+    {
+        $this->issueId = $issueId;
+
+        return $this;
+    }
+
+    /**
+     * Get issueId
+     *
+     * @return integer
+     */
+    public function getIssueId()
+    {
+        return $this->issueId;
+    }
+
+
+
+    /**
+     * Set projectId
+     *
+     * @param integer $projectId
+     * @return Notifications
+     */
+    public function setProjectId($projectId)
+    {
+        $this->projectId = $projectId;
+
+        return $this;
+    }
+
+    /**
+     * Get projectId
+     *
+     * @return integer
+     */
+    public function getProjectId()
+    {
+        return $this->projectId;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Notifications
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set isRead
+     *
+     * @param boolean $isRead
+     * @return Notifications
+     */
+    public function setIsRead($isRead)
+    {
+        $this->isRead = $isRead;
+
+        return $this;
+    }
+
+    /**
+     * Get isRead
+     *
+     * @return boolean 
+     */
+    public function getIsRead()
+    {
+        return $this->isRead;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Notifications
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     * @return Notifications
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return \DateTime 
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+}
